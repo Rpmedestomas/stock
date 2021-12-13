165406e61d6c88050bfc675267967e27f0ea17bb
Arwie Mendoza <arwie@avionschool.com>
2021-12-13 12:47:00 +0800

    Added validations on User.rb

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,8 @@
 class User < ApplicationRecord
-    validates :name, presence: true,
-        
+    validates :name, presence: true
+    validates :email, presence: true, uniqueness: true
+    validates :password, presence: true, uniqueness: {case_sensitive: true}, length: {minimum: 8}
+    validates :confirm_password, presence: true
+    validates :user_type, presence: true
+    validates :status, presence: true
 end
