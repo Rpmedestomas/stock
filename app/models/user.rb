8ff182af94b03b94d16b20c994808fe334401deb
Rpmedestomas <rpmedestomas@gmail.com>
2021-12-13 13:01:21 +0800

    created 1 userand removed uniqueness validation for password

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ApplicationRecord
     validates :name, presence: true
     validates :email, presence: true, uniqueness: true
-    validates :password, presence: true, uniqueness: {case_sensitive: true}, length: {minimum: 8}
+    validates :password, presence: true, length: {minimum: 8}
     validates :confirm_password, presence: true
     validates :user_type, presence: true
     validates :status, presence: true
